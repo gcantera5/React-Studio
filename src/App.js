6edc7335a893a2a609c1b42b641ce662f5210fc7
gcantera5 <guadalupe_cantera@brown.edu>
2024-03-14 18:37:25 -0400

    saving

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
-import { useState } from "react";
+import {useState, useEffect } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem.js";
+
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,19 +13,40 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [bakeryItems, setBakeryItems] = useState(bakeryData);
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const loadData = () => {
+    setBakeryItems(bakeryData);
+  };
+
+  useEffect(() => {
+    // Loads data
+    loadData();
+  }, [cartItems]); // updates cart items 
+
+  // Add item to our cart
+  const addToCart = (price, name) => {
+      setTotalPrice((prevTotal) => prevTotal + price);
+      setCartItems((prevCart) => [...prevCart, name]);
+  };
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1>Levi's Bakery</h1> 
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
+      {bakeryItems.map((item, index) => (
+                <BakeryItem key={index} item={item} addToCart={addToCart} />
+            ))}
       <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+          <h2>Cart</h2>
+          Your Total: ${totalPrice}
+          {cartItems.map((name, index) => (
+          <p key={index}>{name}</p>
+          ))}
       </div>
+
     </div>
   );
 }
